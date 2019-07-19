cf404ed7d961036a540d2948a465d8717b52f726
Trish Ogen <patriciaogen@gmail.com>
2019-07-19 02:17:33 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -50,12 +50,12 @@ def can_find_the_first_item_from_the_database_using_id
   Movie.first!
 end
 
-def can_find_by_multiple_attributes
+def can_find_by_multiple_attributes(args)
   # Search Values:
   # title == "Title"
   # release_date == 2000
   # director == "Me"
-  Movie.find_by!
+  Movie.find_by!(args)
 end
 
 def can_find_using_where_clause_and_be_sorted
