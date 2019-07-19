5ab9bd1d9ec40bd525f27d15c1f82329fa3d2238
Trish Ogen <patriciaogen@gmail.com>
2019-07-19 01:56:27 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -12,4 +12,12 @@
 
 ActiveRecord::Schema.define(version: 2019_07_19_015303) do
 
+  create_table "movies", force: :cascade do |t|
+    t.string "title"
+    t.integer "release_date"
+    t.string "director"
+    t.string "lead"
+    t.boolean "in_theaters"
+  end
+
 end
